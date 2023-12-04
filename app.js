954d4dd2d6008ae209bb02581d0967f2f56b4ceb
zhongsijia5726@outlook.com <zhong.sij@northeastern.edu>
2023-12-04 04:01:31 -0800

    a6 mogo

--- app.js
+++ app.js
@@ -1,23 +1,45 @@
 import express from 'express';
 import Hello from "./hello.js";
 import Lab5 from "./lab5.js";
-import cors from "cors";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import AssignmentRoutes from './assignments/routes.js';
 import "dotenv/config";
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
+import cors from "cors";
+import session from "express-session";
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 
 
 const app = express()
-app.use(cors());
+// app.use(cors());
 app.use(
     cors({
       credentials: true,
       origin: process.env.FRONTEND_URL
     })
   );  
-app.use(express.json());
 
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
+app.use(session(sessionOptions));
+
+app.use(
+  session(sessionOptions)
+);
+app.use(express.json());
+UserRoutes(app);
 AssignmentRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);

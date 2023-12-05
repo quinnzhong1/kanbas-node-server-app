7855afd75fca45675ce888a93342d89ae052a90e
zhongsijia5726@outlook.com <zhong.sij@northeastern.edu>
2023-12-04 17:27:52 -0800

    Update a6 3

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const app = express()
 app.use(
     cors({
       credentials: true,
-      origin: process.env.FRONTEND_URL
+      origin: 'https://a6--vocal-cucurucho-0cbaf0.netlify.app/'
     })
   );  
 

f17a37d8fb784f201c4128282ffae7d89364e04a
zhongsijia5726@outlook.com <zhong.sij@northeastern.edu>
2023-12-04 16:54:54 -0800

    Update a6

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ import mongoose from "mongoose";
 import UserRoutes from "./users/routes.js";
 import cors from "cors";
 import session from "express-session";
-mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
+mongoose.connect(CONNECTION_STRING);
 
 
 const app = express()
